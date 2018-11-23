b1805642be10b1866c46e00a9f442c8aa784bdb1
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-11-23 18:15:36 +0700

    First commit

---
+++ Songs/Migrations/20181122022203_Add-property-Song.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Songs.Migrations
+{
+    public partial class AddpropertySong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Songs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Songs");
+        }
+    }
+}
